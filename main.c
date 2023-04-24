839c216cb1d1b1865818317f83b0f18387302d0f
= <=>
2023-04-24 11:36:08 -0400

    adds cousin section of findFullConsanguinity

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 typedef struct node {
     struct node* parent;
@@ -35,7 +36,7 @@ void selectMembers(relative *node_1, relative *node_2, size_t family_size) {
         char input[5];
         
         printf("Please select a person: ");
-        int error = fgets(&input, 5, stdin);
+        char *error = fgets(input, 5, stdin);
         
         int value;
         
@@ -57,9 +58,11 @@ void selectMembers(relative *node_1, relative *node_2, size_t family_size) {
         char input[5];
         
         printf("Please select another person: ");
-        int error = fgets(&input, 5, stdin);
+        char *error = fgets(input, 5, stdin);
+        printf("hello");
         
         int value;
+        
         
         if ((error != EOF && is_int(input))) {
             sscanf(input, "%d", &value);
@@ -74,6 +77,7 @@ void selectMembers(relative *node_1, relative *node_2, size_t family_size) {
             printf("Error: Invalid Input \n");
         }
     }
+
 }
 
 void nodeTraversal(relative *node, int traversalHops) {
@@ -87,6 +91,8 @@ void nodeTraversal(relative *node, int traversalHops) {
 int markSearch(relative *node, int *nodeHops, int searchHops) {
     if (node->isMarked) {
         *nodeHops = node->hops;
+        printf("%i\n", node->hops);
+        printf("%i\n", searchHops);
         return searchHops;
     } else return markSearch(node->parent, nodeHops, searchHops + 1);
 }
@@ -96,10 +102,31 @@ void findGeneration(char *generation, int depth) {
     if (genDiff == 1) strcat(generation, "grand");
     else if (genDiff > 1) {
         char *greats = malloc((genDiff * 13) * sizeof(char));
+        *generation = malloc((genDiff * 13) * sizeof(char));
         for (int i = 0; i < genDiff - 1; i++) strcat(greats, "great ");
-        *generation = strcat(greats, "grand");
+        strcpy(generation, strcat(greats, "grand"));
+        free(greats);
     } else {
         *generation = malloc(7 * sizeof(char));
+    }
+}
+
+char *get_ordinal (char **ordinals, int value)
+{
+    value %= 100;  /* normalize values between 0-100 */
+
+    if (3 < value && value < 21)
+        return ordinals[3];
+
+    switch (value % 10) {
+        case 1 :    return ordinals[0];
+                    break;
+        case 2 :    return ordinals[1];
+                    break;
+        case 3 :    return ordinals[2];
+                    break;
+        default:    return ordinals[3];
+                    break;
     }
 }
 
@@ -109,9 +136,10 @@ void findFullConsanguinity(int partial_consanguinity, int depth, char *return_re
             if (depth == 0) *return_relationship = "self";
             else {
                 char *generation;
-                findGeneration(&generation, depth);
-                if (depth < 0) *return_relationship = strcat(generation, "child");
-                else if (depth > 0) *return_relationship = strcat(generation, "parent");
+                findGeneration(generation, depth);
+                if (depth < 0) strcpy(return_relationship, strcat(generation, "child"));
+                else if (depth > 0) strcpy(return_relationship, strcat(generation, "parent"));
+                free(generation);
             }
             break;
         case 2: 
@@ -119,13 +147,18 @@ void findFullConsanguinity(int partial_consanguinity, int depth, char *return_re
             else {
                 char *generation;
                 findGeneration(&generation, depth);
-                if (depth < 0) *return_relationship = strcat(generation, "niece/nephew");
-                else if (depth > 0) *return_relationship = strcat(generation, "uncle/aunt");
+                if (depth < 0) strcpy(return_relationship, strcat(generation, "niece/nephew"));
+                else if (depth > 0) strcpy(return_relationship, strcat(generation, "uncle/aunt"));
+                free(generation);
             }
             break;
         default:
             if (partial_consanguinity > 2 && partial_consanguinity % 2 == 0) {
                 int cousin_number = partial_consanguinity - (partial_consanguinity/2 + 1);
+                int times_removed = abs(depth);
+                char *ordinals[] = { "st", "nd", "rd", "th" };
+                if (times_removed == 0) sprintf(return_relationship, "%i%s cousin", cousin_number, get_ordinal(ordinals, cousin_number));
+                else sprintf(return_relationship, "%i%s cousin, %ix removed", cousin_number, get_ordinal(ordinals, cousin_number), times_removed);
             }
             break;
     }
@@ -144,7 +177,7 @@ int main(void) {
     relative second_member;
     
     selectMembers(&first_member, &second_member, family_size + 1);
-
+    
     int nodeHops;
     nodeTraversal(&first_member, 0);
     
@@ -154,6 +187,12 @@ int main(void) {
     int depth = nodeHops - searchHops;
     
     int partial_consanguinity = (nodeHops + searchHops) - depth;
+    
+    char return_relationship[29];
+    
+    findFullConsanguinity(partial_consanguinity, depth, return_relationship);
+    
+    printf("%s", return_relationship);
 
     return 0;
 }
